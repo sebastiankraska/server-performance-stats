7c43c4e17d4443a3f250420c61e778c083c7f4f6
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-09-15 07:16:31 +0200

    Add memory information retrieval (no percentage yet)

--- server-stats.sh
+++ server-stats.sh
@@ -2,5 +2,5 @@ lscpu |
     grep ".*CPU(s).*:*[0-9]\+%" | # ensures it works in different locales (tested with en, de)
     grep -o '[0-9]\+%' # get the cpu usage percentage only
 
-
+cat /proc/meminfo | grep -E "MemTotal|MemFree|MemAvailable" # TODO: get percentage
 
