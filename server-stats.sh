e26013511db4b8269e58a771c1007ee984e885a1
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-09-15 07:34:42 +0200

    Refactor memory information retrieval to store and display MemTotal, MemFree, and MemAvailable values

--- server-stats.sh
+++ server-stats.sh
@@ -2,5 +2,11 @@ lscpu |
     grep ".*CPU(s).*:*[0-9]\+%" | # ensures it works in different locales (tested with en, de)
     grep -o '[0-9]\+%' # get the cpu usage percentage only
 
-cat /proc/meminfo | grep -E "MemTotal|MemFree|MemAvailable" # TODO: get percentage
+meminfo=$(grep -E "MemTotal|MemFree|MemAvailable" /proc/meminfo)
+MemTotal=$(echo "$meminfo" | grep MemTotal | awk '{print $2}')
+MemFree=$(echo "$meminfo" | grep MemFree | awk '{print $2}')
+MemAvailable=$(echo "$meminfo" | grep MemAvailable | awk '{print $2}')
 
+echo $MemTotal
+echo $MemFree
+echo $MemAvailable
